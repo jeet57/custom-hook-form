701100fade34927b16094b0336cf17aa98539f9d
jeet57 <jeet.mishra57@gmail.com>
2023-09-15 13:21:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import useFormInput from "./useFormInput";
+
+export default function App() {
+  const firstNameProps = useFormInput("Mary");
+  const lastNameProps = useFormInput("Poppins");
+
+  return (
+    <>
+      <label>
+        First name:
+        <input {...firstNameProps} />
+      </label>
+      <label>
+        Last name:
+        <input {...lastNameProps} />
+      </label>
+      <p>
+        <b>
+          Good morning, {firstNameProps.value} {lastNameProps.value}.
+        </b>
+      </p>
+    </>
+  );
+}
